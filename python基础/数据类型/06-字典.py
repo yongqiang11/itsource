5023eaa6a8724ef1e90340332f458e5aef55fbfc
罗永强 <yongqiang020@126.com>
2023-03-23 22:25:53 +0800

    2023-03-23

--- python基础/数据类型/06-字典.py
+++ python基础/数据类型/06-字典.py
@@ -48,7 +48,7 @@ info = {'name': '李四', 'age': 20}
 del info['name']
 print(info)
 
-# 语法 字典.pop(key) 删除指定的key并返回相应的value，不存在就会报错
+# 语法 字典.pop(key) 删除指定的key并弹出相应的value，不存在就会报错
 
 info = {'name': '李四', 'age': 20}
 print(info.pop('name'))
@@ -58,4 +58,74 @@ print(info)
 info = {'name': '李四', 'age': 20}
 info.clear()
 print(info)
+print('==' * 20)
 
+# TODO 字典查询
+# 字典[key]  通过key获取对应的值,如果不存在，就会报错
+info1 = {'name': '李四', 'age': 20}
+print(info1['age'])
+# print(info1['addr'])
+
+print('==' * 20)
+# dict.get(key[, value]) 返回指定键的值，如果键不在字典中返回默认值 None 或者设置的默认值。
+print(info1.get('name'))
+print(info1.get('adr'))
+print(info1.get('nam', '四川'))
+
+# len(容器)  str, list, tuple, dict 获取容器的长度
+print(len(info1))
+
+print('--' * 30)
+# dict.values()  获取字典的值
+print(info1.values())
+# dict.keys()   获取字典的键
+print(info1.keys())
+# dict.items()  获取所有的key和value
+print(info1.items())
+
+print('--' * 30)
+# in 语句
+# 元素是否在容器中 是就是True 否就是False
+# 语法： 元素 in 容器
+dict1 = {'name': 'hello', 'age': 18}
+# 查看key在不在字典中
+# 第一种
+print('name' in dict1.keys())
+# 第二种
+print('name' in dict1)
+# 查看value在不在字典中
+print('hello' in dict1.values())
+
+# list
+print('hello' in ['hello', 'good'])
+print('hello1' in ['hello', 'good'])
+
+print('--' * 30)
+# TODO 字典-遍历
+# 遍历key
+# di
+dict1 = {'name': 'hello', 'age': 18}
+# 第一种
+for key in dict1.keys():
+    print(key)
+
+print('==' * 30)
+# 第二种
+for key in dict1:
+    print(key)
+
+print('==' * 30)
+
+# 遍历value
+
+for value in dict1.values():
+    print(value)
+
+print('==' * 30)
+
+# 遍历value和key
+# for item in dict1.items():
+#     print(item)         无法实现，可以使用元组拆包
+
+for key, value in dict1.items():
+    print(key, value)
---
+++ python基础/数据类型/08-公共方法.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : 08-公共方法.py
+# Time       ：2023/3/23 22:17
+# Author     ：ant
+# version    ：python 3.11
+# Description：
+"""
+# TODO 内置函数
+# len()  获取容器的长度    str,list,tuple,dict,set
+print(len('hello'))
+print(len((1, 2, 3, 4)))
+print(len([1, 2, 3, 4]))
+print(len({1, 2, 3, 4}))
+print(len({'num': 1, 'num1': 2, 'num3': 3}))
+# del()  删除变量
+# max()   获取容器的最大值
+# min()   获取容器的最小值
