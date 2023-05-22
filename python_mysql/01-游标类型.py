d4b6533d2dd4e9bbc67fccfd793b8700edcdf9ac
罗永强 <yongqiang020@126.com>
2023-05-22 17:55:26 +0800

    python cursor

---
+++ python_mysql/01-游标类型.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : 01-游标类型.py
+# Time       ：2023/5/22 17:55
+# Author     ：ant
+# version    ：python 3.11
+# Description：
+"""
--- python高级-02/test_data.py
+++ python高级-02/test_data.py
@@ -8,30 +8,10 @@
 # version    ：python 3.11
 # Description：
 """
-import datetime
 
+list1 = {'1': 1, '2': 2}
+list2 = list1
+list1['1'] = 5
+sum1 = list1['1'] + list2['1']
+print(sum1)
 
-a = 1
-b = 2
-c = (3, 5)
-
-# 装包
-
-tuple1 = a, b, *c
-print(type(tuple1))
-print(tuple1)
-list2 = list(tuple1)
-print(list2)
-
-set1 = set(tuple1)
-print(set1)
-print(type(set1))
-#
-# dict1 = dict(tuple1)
-# print(dict1)
-# print(type(dict1))
-
-print('==' * 20)
-list1 = [1, 2, 3, 4]
-c, d, *e = list1
-print(c, d, e)
